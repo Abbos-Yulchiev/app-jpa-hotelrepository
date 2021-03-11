f8c05aa5946f37cde8befe39868802f2fa21d9e6
Abbos <yulchiyevabbos1@gmail.com>
2021-03-11 20:11:18 +0500

    firstCommit

---
+++ src/main/java/uz/pdp/appjpahotelrepository/AppJpaHotelrepositoryApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.appjpahotelrepository;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppJpaHotelrepositoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppJpaHotelrepositoryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/appjpahotelrepository/AppJpaHotelrepositoryApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.appjpahotelrepository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppJpaHotelrepositoryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
